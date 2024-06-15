30073cd056cadeb8671c6b83918e656067b2683c
Jesse van den Kieboom <JKieboom@esri.com>
2024-06-15 11:45:38 +0200

    fmt

--- termsize.go
+++ termsize.go
@@ -1,3 +1,4 @@
+//go:build !windows && !plan9 && !appengine && !wasm && !aix
 // +build !windows,!plan9,!appengine,!wasm,!aix
 
 package flags
--- termsize_nosysioctl.go
+++ termsize_nosysioctl.go
@@ -1,3 +1,4 @@
+//go:build plan9 || appengine || wasm || aix
 // +build plan9 appengine wasm aix
 
 package flags
